694bd158ead0d2a420ddd7b0a910df201cdc3df7
harelagung <harel.nugroho29@gmail.com>
2025-11-18 19:57:10 +0700

    change password for fake user ( pass123 )

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -28,7 +28,7 @@ class UserFactory extends Factory
             "username" => fake()->userName(),
             "email" => fake()->unique()->safeEmail(),
             "email_verified_at" => now(),
-            "password" => (static::$password ??= Hash::make("password")),
+            "password" => (static::$password ??= Hash::make("pass123")),
             "remember_token" => Str::random(10),
         ];
     }
